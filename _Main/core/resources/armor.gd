b2a905e2752f0f9e2fd63cd7e79c90443f2540fc
Bautista Di Santo <juanbautistadisanto@gmail.com>
2026-04-02 19:20:08 -0300

    add inventory UI & update armor res

--- _Main/core/resources/armor.gd
+++ _Main/core/resources/armor.gd
@@ -2,12 +2,18 @@ extends Resource
 class_name Armor
 
 enum ARMOR_TYPE {
-	hand,
-	arms,
+	head,
 	torso,
 	legs
 }
 
 @export var mesh: Resource
-@export var physical_resistance: float
+@export var name: String
 @export var armor_type: ARMOR_TYPE
+@export var weight: float
+@export_group("resistances")
+@export var physical_resistance: float
+@export var magic_resistance: float
+@export var fire_resistance: float
+@export var freeze_resistance: float
+@export var poison_resistance: float
